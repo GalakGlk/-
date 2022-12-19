b91decd02cf676a3c1d2d1807bf5247f283f7464
GalakGlk <galak7premium@gmail.com>
2022-12-20 02:19:52 +0300

    1
    1

---
+++ WindowsFromsApp30/WindowsFormsApp30/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WindowsFormsApp30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog3 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 497);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 535);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(585, 61);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(437, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 21);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 20);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Заливка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 49);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фигура";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(162, 20);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(105, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Прямоугольник";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(69, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Окружность";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X:0; Y:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 595);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Графический редактор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.ColorDialog colorDialog3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ WindowsFromsApp30/WindowsFormsApp30/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp30
+{
+    public partial class Form1 : Form
+        
+    {
+        
+        Pen pRed = new Pen(Color.Red, 3);
+        Brush bBrown = new SolidBrush(Color.Brown);
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Height - 85;
+            panel1.Width = this.Width;
+            button1.Click += button1_Click;
+            colorDialog1.FullOpen = true;
+            colorDialog1.Color = this.BackColor;
+        }
+
+        Point tempPoint;
+        Boolean tempClick = false;
+        Pen pen = new Pen(Color.Red, 2);
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Height - 85;
+            panel1.Width = this.Width;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = $"X:{e.X}; Y:{e.Y}";
+        }
+
+        
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (tempClick == false)
+            {
+                tempClick = true;
+                tempPoint.X = e.X;
+                tempPoint.Y = e.Y;
+            }
+            else
+            {
+                if (radioButton1.Checked == true)
+                {
+                    tempClick = false;
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bmp;
+                    Graphics g = Graphics.FromImage(bmp);
+
+                    g.DrawLine(pen, tempPoint.X, tempPoint.Y, e.X, e.Y);
+
+                   
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    tempClick = false;
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bmp;
+                    Graphics g = Graphics.FromImage(bmp);
+
+                    int w = Math.Abs(e.X - tempPoint.X);
+                    int h = Math.Abs(e.Y - tempPoint.Y);
+
+
+                    g.FillEllipse(bBrown, tempPoint.X, tempPoint.Y, w, h);
+                    g.DrawEllipse(pRed, tempPoint.X, tempPoint.Y, w, h);
+                }
+                else if (radioButton3.Checked == true)
+                {
+                    tempClick = false;
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bmp;
+                    Graphics g = Graphics.FromImage(bmp);
+                   // int t = Math.Abs(e.X - tempPoint.X);
+                    //int j = Math.Abs(e.Y - tempPoint.Y);
+                    //int y = Math.Abs(e.S - tempPoint.S);
+                    //int l = Math.Abs(e.H - tempPoint.H);
+                    g.FillRectangle(bBrown, tempPoint.X, tempPoint.Y, 200,100);
+                    //g.DrawEllipse(pRed,x,y,t,j );
+                    g.DrawRectangle( pRed, tempPoint.X, tempPoint.Y, 200, 100);
+                }
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            pen.Color = colorDialog1.Color;
+           
+            pRed.Color = colorDialog1.Color;
+            BackColor = colorDialog1.Color;
+
+
+
+
+
+
+
+
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            bBrown = new SolidBrush(colorDialog1.Color);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить карнтинку как....";
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true;
+                sfd.Filter = "Image Files(*.BMP|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|" + "" +
+                    "Image Files(*.PNG)|*.PNG|All files(*.*)|*.*";
+                sfd.ShowHelp = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(sfd.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|" + "" +
+                    "Image Files(*.PNG)|*.PNG|All files(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбраннок изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+           // PictureBox pictureBox2 =new PictureBox
+            //{
+//Size=pictureBox1.Size,Location=new Point(0,0),BackColor=Color.Transparent,Image=Resources.neddedImage
+           // }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
